048d75b8437dc56f29a66ae401efa37afff614d6
RimmaBuzuluk <rimmysya2000@gmail.com>
2024-03-04 03:04:05 +0200

    reduser auth

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import typeFilter from './slice/typeFilter';
+import { authReducer } from './slice/auth';
 
 const rootReducer = combineReducers({
 	typeFilter: typeFilter,
+	auth: authReducer,
 });
 
 const store = configureStore({
