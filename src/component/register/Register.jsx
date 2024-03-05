d4d1b72c0961e2b5a27a81153180e5a2266a4a48
RimmaBuzuluk <rimmysya2000@gmail.com>
2024-03-05 21:10:26 +0200

    registration

--- src/component/register/Register.jsx
+++ src/component/register/Register.jsx
@@ -1,28 +1,84 @@
+import React, { useState } from 'react';
 import './Register.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRegister, selectIsAuth } from '../../store/slice/auth';
+import { Navigate } from 'react-router-dom';
 
 function Register() {
+	const dispatch = useDispatch();
+	const isAuth = useSelector(selectIsAuth);
+
+	const [name, setName] = useState('Римма Бузулук');
+	const [email, setEmail] = useState('buzuluk@gmail.com');
+	const [number, setNumber] = useState('0953272213');
+	const [address, setAddress] = useState('lalal');
+	const [password, setPassword] = useState('12345678');
+
+	const handleNameChange = e => {
+		setName(e.target.value);
+	};
+
+	const handleEmailChange = e => {
+		setEmail(e.target.value);
+	};
+
+	const handlePhoneChange = e => {
+		setNumber(e.target.value);
+	};
+
+	const handleAddressChange = e => {
+		setAddress(e.target.value);
+	};
+
+	const handlePasswordChange = e => {
+		setPassword(e.target.value);
+	};
+
+	const onSubmit = async () => {
+		const values = { name, number, address, email, password };
+		const data = await dispatch(fetchRegister(values));
+
+		// console.log(data);
+
+		// if (!data.payload) {
+		// 	return alert('не вдалось зареєструватись');
+		// }
+
+		// if ('token' in data.payload) {
+		// 	window.localStorage.setItem('token', data.payload.token);
+		// }
+	};
+
+	if (isAuth) {
+		return <Navigate to='/' />;
+	}
+
 	return (
 		<div className='Registration'>
 			<div className='registration_items'>
+				<div>Registration</div>
 				<div className='register_item'>
 					<div className='register_itemName'>Name:</div>
-					<input />
+					<input value={name} onChange={handleNameChange} />
 				</div>
 				<div className='register_item'>
 					<div className='register_itemName'>Email:</div>
-					<input />
+					<input value={email} onChange={handleEmailChange} />
 				</div>
 				<div className='register_item'>
 					<div className='register_itemName'>Phone:</div>
-					<input />
+					<input value={number} onChange={handlePhoneChange} />
 				</div>
 				<div className='register_item'>
 					<div className='register_itemName'>Address:</div>
-					<input />
+					<input value={address} onChange={handleAddressChange} />
 				</div>
 				<div className='register_item'>
 					<div className='register_itemName'>Password:</div>
-					<input />
+					<input value={password} onChange={handlePasswordChange} />
+				</div>
+				<div className='registerButt'>
+					<button onClick={onSubmit}>OK</button>
 				</div>
 			</div>
 		</div>
